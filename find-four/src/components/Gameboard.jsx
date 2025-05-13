def3892e452173528a22ab499c7b6fd6774b488d
Ashley Mullin <aetmullin@hotmail.com>
2025-05-13 18:58:56 +0100

    Started work on adding tile to gameboard

--- find-four/src/components/Gameboard.jsx
+++ find-four/src/components/Gameboard.jsx
@@ -1,13 +1,24 @@
 import Tile from "./Tile"
+import { useState } from "react"
 import { wordData } from "../assets/wordData"
 import './Gameboard.css'
 
 
 export default function Gameboard(){
 
+const [selectedWords, setSelectedWords] = useState([])
+
+function handleTileSelect(word, group){
+    const newWords = [...selectedWords, {
+        group: group,
+        word: word
+    }]
+    setSelectedWords(newWords);
+}
+
     return (
         <div id="gameboard">
-            {wordData.map((words) => words.map((word) => <Tile key={word.word} group={word.key} word={word.word}></Tile>)
+            {wordData.map((words) => words.map((word) => <Tile selectedWords={selectedWords} tileSelect={handleTileSelect} key={word.word} group={word.key} word={word.word}></Tile>)
             )}
         </div>
     )
--- find-four/src/components/Tile.jsx
+++ find-four/src/components/Tile.jsx
@@ -1,12 +1,23 @@
 import './Tile.css'
+import { useState } from 'react';
 
-export default function Tile({word, group}){
+export default function Tile({word, group, tileSelect, selectedWords}){
+
+    const [selected, setSelected] = useState(false)
 
     function handleClick(){
-        console.log(word, group)
+        if (selectedWords.length === 4){
+            console.log("You can only select 4 words!")
+        } else if (selected === true){
+            setSelected(false)
+        } else {
+            setSelected(true)
+            tileSelect(word, group)
+        }
+        
     }
 
     return (
-        <div className="tile" onClick={handleClick}>{word}</div>
+        <div className={`tile ${selected && "selected"}`} onClick={handleClick}>{word}</div>
     )
 }
